8d46b243e5f120ffc811fd8102edb344262e5dfd
Ihar_Blinou <blinov@gmail.com>
2023-01-16 20:34:50 +0300

    6.I

--- src/main/java/com/epam/array/main/DeclaringArrayMain.java
+++ src/main/java/com/epam/array/main/DeclaringArrayMain.java
@@ -7,21 +7,60 @@ import com.epam.array.service.impl.ArrayServiceImpl;
 import com.epam.array.service.impl.ArrayStreamServiceImpl;
 
 import java.util.Arrays;
+import java.util.Optional;
+
+enum Shape {
+  RECTANGLE, TRIANGLE, CIRCLE;
+
+  public double defineSquare(double... x) {
+// here may be checking the parameters for correctness
+    return switch (this) {
+      case RECTANGLE -> x[0] * x[1];
+      case TRIANGLE -> x[0] * x[1] / 2;
+      case CIRCLE -> Math.pow(x[0], 2) * Math.PI;
+      default -> throw new EnumConstantNotPresentException(getDeclaringClass(), name());
+    };
+  }
+}
+
+record Type(int id, StringBuilder filename) {
+  @Override
+  public StringBuilder filename() {
+    return new StringBuilder(filename.toString());
+  }
+}
 
 public class DeclaringArrayMain {
+
+  //static final Integer a= Integer.parseInt("9uuu");
+
   public static void main(String[] args) {
+//    int t = 1;
+//    t = 9;
+//    Type obj = new Type(77, new StringBuilder("abc.txt"));
+//    StringBuilder s = obj.filename().append("uyt");
+//    System.out.println(obj.filename());
+//    System.out.println(obj.id());
+//    System.out.println(obj.toString());
+//    System.out.println(obj.hashCode());
+//    String roleStr = "client11";
+//    roleStr = roleStr.toUpperCase();
+//    System.out.println(roleStr);
+//    roleStr ="t";
+//    int b = 1;
+//
     ArrayReaderImpl reader = new ArrayReaderImpl();
     String filename = "data\\arrays.txt";
     int a[] = reader.readArray(filename);
-    CustomArray customArray = new CustomArray(a);
-    System.out.println(customArray);
-    ArrayService service = new ArrayServiceImpl();
-    int res = service.findMax(customArray);
-    System.out.println(res);
-    service = new ArrayStreamServiceImpl();
-    res = service.findMax(customArray);
-    System.out.println(res);
- //   System.out.println(Arrays.toString(a));
+//    CustomArray customArray = new CustomArray(a);
+//    System.out.println(customArray);
+//    ArrayService service = new ArrayServiceImpl();
+//    int res = service.findMax(customArray);
+//    System.out.println(res);
+//    service = new ArrayStreamServiceImpl();
+//    res = service.findMax(customArray);
+//    System.out.println(res);
+    //   System.out.println(Arrays.toString(a));
 //    int [] array = {1, 2, 3, 4, 5};
 //    int a[] = new int[10];
 //    String strings[] = new String[10];
@@ -31,6 +70,5 @@ public class DeclaringArrayMain {
 //    var n = Integer.parseInt("6667");
 //    var m = Arrays.stream(a).asLongStream().average();
 //    var list = new ArrayList<String>();
-   
   }
 }
--- src/main/java/com/epam/array/reader/impl/ArrayReaderImpl.java
+++ src/main/java/com/epam/array/reader/impl/ArrayReaderImpl.java
@@ -1,6 +1,8 @@
 package com.epam.array.reader.impl;
 
 import com.epam.array.reader.ArrayReader;
+import com.epam.array.validator.StringArrayValidator;
+import com.epam.array.validator.impl.StringArrayValidatorImpl;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -12,31 +14,46 @@ import java.util.Arrays;
 public class ArrayReaderImpl implements ArrayReader {
   private static final String DEFAULT_FILENAME = "data\\arrays.txt";
   public int[] readArray(String filename) {
-    int[] arr = null;
+    int[] arrTemp = null;
     Path path = Path.of(filename);
    if(!Files.exists(path)) {
      System.out.println("file " + filename + " not exist");
      filename = DEFAULT_FILENAME;
    }
-    try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+    BufferedReader reader = null;
+
+    try  {
+       reader = new BufferedReader(new FileReader(filename));
       String tmp;
       if ((tmp = reader.readLine()) != null) { //java 2
         System.out.println(tmp);
         String[] str = tmp.split("\\s+");
         System.out.println(Arrays.toString(str));
         System.out.println(str.length);
-        arr = new int[str.length];
+        arrTemp = new int[str.length];
+        int counter = 0;
+        StringArrayValidator validator = new StringArrayValidatorImpl();
         for (int i = 0; i < str.length; i++) {
-          try {
-            arr[i] = Integer.parseInt(str[i]);
-          } catch (NumberFormatException e) {
-            System.err.println(e);
-          }
+            if(validator.stringNumberValidate(str[i])) {
+              arrTemp[counter] = Integer.parseInt(str[i]);
+              counter++;
+            }
         }
+        System.out.println(Arrays.toString(arrTemp));
+        int [] arrres = Arrays.copyOf(arrTemp, counter);
+        System.out.println(Arrays.toString(arrres));
       }
     } catch (IOException  e) {
       System.err.println(e);
+    } finally {
+      if(reader != null) {
+        try {
+          reader.close();
+        } catch (IOException e) {
+          //
+        }
+      }
     }
-    return arr;
+    return arrTemp;
   }
 }
