ec76b265918cc06e7d6f37d191834ba2af1ad47d
Ihar_Blinou <blinov@gmail.com>
2023-01-23 19:26:40 +0100

    23.I

--- src/main/java/com/epam/learn/main/LocaleMain.java
+++ src/main/java/com/epam/learn/main/LocaleMain.java
@@ -1,5 +1,8 @@
 package com.epam.learn.main;
 
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -8,5 +11,14 @@ public class LocaleMain {
     Locale locale = new Locale("fr");
     ResourceBundle resourceBundle = ResourceBundle.getBundle("res.sweetdata2", locale);
     System.out.println(resourceBundle.getString("submit1"));
+    Date date = new Date();
+    double d = 1_234_567.891;
+    Locale[] locales = DateFormat.getAvailableLocales();
+    for (Locale loc : locales) {
+      DateFormat format = DateFormat.getDateInstance(DateFormat.FULL, loc);
+      NumberFormat numberFormatUs = NumberFormat.getInstance(loc);
+      System.out.print(numberFormatUs.format(d) + "    ^^^^^^^  ");
+      System.out.println(loc.toString() + "---> " + format.format(date));
+    }
   }
 }
