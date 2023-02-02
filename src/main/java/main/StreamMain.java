c9b154766d69a6e7505cef90e1d712c709f51e23
Ihar_Blinou <blinov@gmail.com>
2023-02-02 21:01:40 +0300

    02.II

--- src/main/java/main/StreamMain.java
+++ src/main/java/main/StreamMain.java
@@ -1,6 +1,12 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.IntConsumer;
+import java.util.function.IntPredicate;
+import java.util.function.IntUnaryOperator;
+import java.util.function.Predicate;
 
 public class StreamMain {
   public static void main(String[] args) {
@@ -10,8 +16,8 @@ public class StreamMain {
 //
 //      return n1 * n2;});
 //    System.out.println(res);
-    Arrays.stream(a)
-            .filter(n -> n > 5).forEach(System.out::println);
+//   Arrays.stream(a)
+//           .filter(n -> n > 5).forEach(System.out::println);
             //.limit(5).filter(n -> n > 5).forEach(System.out::println);
             //.map(n -> n * 10).forEach(System.out::println);
             //.distinct().forEach(System.out::println);
@@ -22,6 +28,41 @@ public class StreamMain {
 //            .map(n -> Integer.toString(n))
 //            .flatMap(s -> Arrays.stream(s.split("")))
 //            .forEach(System.out::println);
+    Predicate<Integer> predicate = new Predicate<Integer>() {
+      @Override
+      public boolean test(Integer n) {
+        return n > 5;
+      }
+    };
+    IntPredicate intPredicate = new IntPredicate() {
+      @Override
+      public boolean test(int n) {
+        return n > 5;
+      }
+    };
+//    Arrays.stream(a)
+//            .filter(intPredicate).forEach(System.out::println);
 
+    IntUnaryOperator operator = new IntUnaryOperator() {
+      @Override
+      public int applyAsInt(int operand) {
+        return operand * 10;
+      }
+    };
+
+    List<Integer> integerList = new ArrayList<>();
+    IntConsumer consumer = new IntConsumer() {
+      @Override
+      public void accept(int value) {
+        if (value > 50) {
+          value++;
+        }
+        integerList.add(value);
+        System.out.print(value + " ");
+      }
+    };
+    Arrays.stream(a)
+            .map(operator).forEach(consumer);
+    System.out.println("\n" +integerList);
   }
 }
