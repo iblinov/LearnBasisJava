16e584b7fdab36a3c8ff41f06eae85b78c1c6cbb
Ihar_Blinou <blinov@gmail.com>
2023-10-12 20:48:05 +0300

    12.X

--- src/main/java/edu/itacademy/first/main/ProjectMain.java
+++ src/main/java/edu/itacademy/first/main/ProjectMain.java
@@ -1,11 +1,39 @@
 package edu.itacademy.first.main;
 
 import edu.itacademy.first.entity.Ball;
+import edu.itacademy.first.service.BallService;
 
 class ProjectMain {
   public static void main(String[] args) {
     Ball ball = new Ball(2, "steel","red", 5.2);
-    ball.setRadius(3);
-    System.out.println(ball);
+    Ball ball2 = new Ball(7, "iron","white", 1.0);
+    Ball ball3 = new Ball(1, "steel","red", 3.2);
+    Ball ball4 = new Ball(1, "steel","red", 3.2);
+
+    Ball[] balls = new Ball[4];
+    balls[0] = ball3;
+    balls[1] = ball;
+    balls[2] = ball2;
+    balls[3] = ball4;
+
+  //  balls = null;
+ //   balls[2] = ball;
+
+//    for (int i = 0; i < balls.length; i++) {
+//      System.out.println(balls[i]);
+//    }
+//    int[] numbers = new int[5];
+//    numbers[3] = 56;
+//    for (int i = 0; i < numbers.length; i++) {
+//      System.out.println(numbers[i]);
+//    }
+ //   System.out.println(ball);
+    BallService service = new BallService();
+    double result = service.defineWeightBalls(balls);
+    System.out.println(result);
+    System.out.println((int)result);
+    System.out.println(Math.round(result));
+    int resulColor = service.findBallByColor(balls, "gr");
+    System.out.println(resulColor);
   }
 }
--- src/main/java/edu/itacademy/first/service/BallService.java
+++ src/main/java/edu/itacademy/first/service/BallService.java
@@ -3,8 +3,24 @@ package edu.itacademy.first.service;
 import edu.itacademy.first.entity.Ball;
 
 public class BallService {
-//  public double defineWeightBalls(){
-//
-//  }
-//  public Ball findBallByColor() {}
+  public double defineWeightBalls(Ball[] balls){
+//    if (balls == null) {
+//      return -1;
+//    }
+     double total = 0;
+    for (int i = 0; i < balls.length; i++) {
+      total += balls[i].getWeight();
+    }
+    return total;
+  }
+
+  public int findBallByColor(Ball[] balls, String color) {
+    int total = 0;
+    for (int i = 0; i < balls.length; i++) {
+      if(balls[i].getColor().equals(color)) {
+        total++;
+      }
+    }
+    return total;
+  }
 }
